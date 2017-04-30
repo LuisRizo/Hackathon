747825f2910fef38dd4796311ea1be717c3a8fce
Luis <Luis>
2017-04-30 00:34:33 -0400

    Fixed merging errors

--- app/app/src/main/java/com/example/lyus/myapplication/Register.java
+++ app/app/src/main/java/com/example/lyus/myapplication/Register.java
@@ -8,6 +8,6 @@ public class Register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.id.lactivity_register);
+        setContentView(R.layout.activity_register);
     }
 }
